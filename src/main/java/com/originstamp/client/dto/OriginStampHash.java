4370022c1df3339db6db85df0956e96bace65f57
Thomas Hepp <thomas.hepp@uni-konstanz.de>
2017-05-15 11:28:53 +0200

    updated the hash entities

--- src/main/java/com/originstamp/client/dto/OriginStampHash.java
+++ src/main/java/com/originstamp/client/dto/OriginStampHash.java
@@ -1,6 +1,7 @@
 package com.originstamp.client.dto;
 
 import javax.xml.bind.annotation.*;
+import java.io.Serializable;
 
 /**
  * Created by Thomas on 10.01.17.
@@ -9,64 +10,218 @@ import javax.xml.bind.annotation.*;
  *         result entity which is returned by a rest request for detailed hash information
  */
 @XmlRootElement(name = "body")
-public class OriginStampHash {
+public class OriginStampHash implements Serializable{
     @XmlElement(name = "comment")
     private String comment;
-    @XmlElement(name = "date_created")
-    private String dateCreated;
-    @XmlElement(name = "hash_string")
-    private String hashString;
-    @XmlElement(name = "unique_url")
-    private String uniqueUrl;
-    @XmlElement(name = "email")
-    private Mail email;
     @XmlElement(name = "twitter")
     private Twitter twitter;
-    @XmlElement(name = "multi_seed")
-    private MultiSeed multiSeed;
+    @XmlElement(name = "unique_url")
+    private String uniqueUrl;
+    @XmlElement(name = "hash_string")
+    private String hashString;
+    @XmlElement(name = "email")
+    private Email email;
+    @XmlElement(name = "date_created")
+    private Long dateCreated;
     @XmlElement(name = "single_seed")
     private SingleSeed singleSeed;
+    @XmlElement(name = "multi_seed")
+    private MultiSeed multiSeed;
+    @XmlElement(name = "created")
+    private Boolean created;
 
     @XmlAccessorType(XmlAccessType.FIELD)
     @XmlType(name = "")
-    public static class Mail {
-        @XmlElement(name = "email_hash")
-        private String mailHash;
-        @XmlElement(name = "verified")
-        private String verified;
+    public static class SingleSeed implements Serializable {
+        @XmlElement(name = "block_depth")
+        private Long blockDepth;
+        @XmlElement(name = "seed_hash")
+        private String seedHash;
+        @XmlElement(name = "submit_status")
+        private Integer submitStatus;
+        @XmlElement(name = "timestamp")
+        private Long timestamp;
+        @XmlElement(name = "tx_hash")
+        private String transactionHash;
+        @XmlElement(name = "btc_address")
+        private String bitcoinAddress;
+        @XmlElement(name = "pub_key")
+        private String publicKey;
 
-        public String getMailHash() {
-            return mailHash;
+        public Long getBlockDepth() {
+            return blockDepth;
         }
 
-        public void setMailHash(String mailHash) {
-            this.mailHash = mailHash;
+        public void setBlockDepth(Long blockDepth) {
+            this.blockDepth = blockDepth;
         }
 
-        public String getVerified() {
-            return verified;
+        public String getSeedHash() {
+            return seedHash;
         }
 
-        public void setVerified(String verified) {
-            this.verified = verified;
+        public void setSeedHash(String seedHash) {
+            this.seedHash = seedHash;
+        }
+
+        public Integer getSubmitStatus() {
+            return submitStatus;
+        }
+
+        public void setSubmitStatus(Integer submitStatus) {
+            this.submitStatus = submitStatus;
+        }
+
+        public Long getTimestamp() {
+            return timestamp;
+        }
+
+        public void setTimestamp(Long timestamp) {
+            this.timestamp = timestamp;
+        }
+
+        public String getTransactionHash() {
+            return transactionHash;
+        }
+
+        public void setTransactionHash(String transactionHash) {
+            this.transactionHash = transactionHash;
+        }
+
+        public String getBitcoinAddress() {
+            return bitcoinAddress;
+        }
+
+        public void setBitcoinAddress(String bitcoinAddress) {
+            this.bitcoinAddress = bitcoinAddress;
+        }
+
+        public String getPublicKey() {
+            return publicKey;
+        }
+
+        public void setPublicKey(String publicKey) {
+            this.publicKey = publicKey;
         }
     }
 
     @XmlAccessorType(XmlAccessType.FIELD)
     @XmlType(name = "")
-    public static class Twitter {
+    public static class MultiSeed implements Serializable {
+        @XmlElement(name = "block_depth")
+        private Long blockDepth;
+        @XmlElement(name = "seed_hash")
+        private String seedHash;
         @XmlElement(name = "submit_status")
-        private int submitStatus;
-        @XmlElement(name = "tweet_id")
-        private String tweetId;
+        private Integer submitStatus;
+        @XmlElement(name = "timestamp")
+        private Long timestamp;
+        @XmlElement(name = "tx_hash")
+        private String transactionHash;
+        @XmlElement(name = "btc_address")
+        private String bitcoinAddress;
+        @XmlElement(name = "pub_key")
+        private String publicKey;
 
-        public int getSubmitStatus() {
+        public Long getBlockDepth() {
+            return blockDepth;
+        }
+
+        public void setBlockDepth(Long blockDepth) {
+            this.blockDepth = blockDepth;
+        }
+
+        public String getSeedHash() {
+            return seedHash;
+        }
+
+        public void setSeedHash(String seedHash) {
+            this.seedHash = seedHash;
+        }
+
+        public Integer getSubmitStatus() {
             return submitStatus;
         }
 
-        public void setSubmitStatus(int submitStatus) {
+        public void setSubmitStatus(Integer submitStatus) {
             this.submitStatus = submitStatus;
         }
+
+        public Long getTimestamp() {
+            return timestamp;
+        }
+
+        public void setTimestamp(Long timestamp) {
+            this.timestamp = timestamp;
+        }
+
+        public String getTransactionHash() {
+            return transactionHash;
+        }
+
+        public void setTransactionHash(String transactionHash) {
+            this.transactionHash = transactionHash;
+        }
+
+        public String getBitcoinAddress() {
+            return bitcoinAddress;
+        }
+
+        public void setBitcoinAddress(String bitcoinAddress) {
+            this.bitcoinAddress = bitcoinAddress;
+        }
+
+        public String getPublicKey() {
+            return publicKey;
+        }
+
+        public void setPublicKey(String publicKey) {
+            this.publicKey = publicKey;
+        }
+    }
+
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "")
+    public static class Email implements Serializable {
+        @XmlElement(name = "email_hash")
+        private String emailHash;
+        @XmlElement(name = "verified")
+        private Integer verified;
+        @XmlElement(name = "send_status")
+        private Integer sendStatus;
+
+        public String getEmailHash() {
+            return emailHash;
+        }
+
+        public void setEmailHash(String emailHash) {
+            this.emailHash = emailHash;
+        }
+
+        public Integer getVerified() {
+            return verified;
+        }
+
+        public void setVerified(Integer verified) {
+            this.verified = verified;
+        }
+
+        public Integer getSendStatus() {
+            return sendStatus;
+        }
+
+        public void setSendStatus(Integer sendStatus) {
+            this.sendStatus = sendStatus;
+        }
+    }
+
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "")
+    public static class Twitter implements Serializable {
+        @XmlElement(name = "tweet_id")
+        private String tweetId;
+        @XmlElement(name = "submit_status")
+        private Integer submitStatus;
 
         public String getTweetId() {
             return tweetId;
@@ -75,57 +230,6 @@ public class OriginStampHash {
         public void setTweetId(String tweetId) {
             this.tweetId = tweetId;
         }
-    }
-
-    @XmlAccessorType(XmlAccessType.FIELD)
-    @XmlType(name = "")
-    public static class MultiSeed {
-        @XmlElement(name = "block_depth")
-        private Integer blockDepth;
-        @XmlElement(name = "seed_hash")
-        private String seedHash;
-        @XmlElement(name = "submit_status")
-        private Integer submitStatus;
-        @XmlElement(name = "timestamp")
-        private String timestamp;
-        @XmlElement(name = "tx_hash")
-        private String transactionHash;
-        @XmlElement(name = "btc_address")
-        private String bitCoinAddress;
-        @XmlElement(name = "pub_key")
-        private String publicKey;
-
-        public String getBitCoinAddress() {
-            return bitCoinAddress;
-        }
-
-        public void setBitCoinAddress(String bitCoinAddress) {
-            this.bitCoinAddress = bitCoinAddress;
-        }
-
-        public String getPublicKey() {
-            return publicKey;
-        }
-
-        public void setPublicKey(String publicKey) {
-            this.publicKey = publicKey;
-        }
-
-        public Integer getBlockDepth() {
-            return blockDepth;
-        }
-
-        public void setBlockDepth(Integer blockDepth) {
-            this.blockDepth = blockDepth;
-        }
-
-        public String getSeedHash() {
-            return seedHash;
-        }
-
-        public void setSeedHash(String seedHash) {
-            this.seedHash = seedHash;
-        }
 
         public Integer getSubmitStatus() {
             return submitStatus;
@@ -134,98 +238,8 @@ public class OriginStampHash {
         public void setSubmitStatus(Integer submitStatus) {
             this.submitStatus = submitStatus;
         }
-
-        public String getTimestamp() {
-            return timestamp;
-        }
-
-        public void setTimestamp(String timestamp) {
-            this.timestamp = timestamp;
-        }
-
-        public String getTransactionHash() {
-            return transactionHash;
-        }
-
-        public void setTransactionHash(String transactionHash) {
-            this.transactionHash = transactionHash;
-        }
     }
 
-    @XmlAccessorType(XmlAccessType.FIELD)
-    @XmlType(name = "")
-    public static class SingleSeed {
-        @XmlElement(name = "block_depth")
-        private Integer blockDepth;
-        @XmlElement(name = "seed_hash")
-        private String seedHash;
-        @XmlElement(name = "submit_status")
-        private Integer submitStatus;
-        @XmlElement(name = "timestamp")
-        private String timestamp;
-        @XmlElement(name = "tx_hash")
-        private String transactionHash;
-        @XmlElement(name = "btc_address")
-        private String bitCoinAddress;
-        @XmlElement(name = "pub_key")
-        private String publicKey;
-
-        public String getBitCoinAddress() {
-            return bitCoinAddress;
-        }
-
-        public void setBitCoinAddress(String bitCoinAddress) {
-            this.bitCoinAddress = bitCoinAddress;
-        }
-
-        public String getPublicKey() {
-            return publicKey;
-        }
-
-        public void setPublicKey(String publicKey) {
-            this.publicKey = publicKey;
-        }
-
-        public Integer getBlockDepth() {
-            return blockDepth;
-        }
-
-        public void setBlockDepth(Integer blockDepth) {
-            this.blockDepth = blockDepth;
-        }
-
-        public String getSeedHash() {
-            return seedHash;
-        }
-
-        public void setSeedHash(String seedHash) {
-            this.seedHash = seedHash;
-        }
-
-        public Integer getSubmitStatus() {
-            return submitStatus;
-        }
-
-        public void setSubmitStatus(Integer submitStatus) {
-            this.submitStatus = submitStatus;
-        }
-
-        public String getTimestamp() {
-            return timestamp;
-        }
-
-        public void setTimestamp(String timestamp) {
-            this.timestamp = timestamp;
-        }
-
-        public String getTransactionHash() {
-            return transactionHash;
-        }
-
-        public void setTransactionHash(String transactionHash) {
-            this.transactionHash = transactionHash;
-        }
-    }
 
     public String getComment() {
         return comment;
@@ -233,38 +247,6 @@ public class OriginStampHash {
 
     public void setComment(String comment) {
         this.comment = comment;
-    }
-
-    public String getDateCreated() {
-        return dateCreated;
-    }
-
-    public void setDateCreated(String dateCreated) {
-        this.dateCreated = dateCreated;
-    }
-
-    public String getHashString() {
-        return hashString;
-    }
-
-    public void setHashString(String hashString) {
-        this.hashString = hashString;
-    }
-
-    public String getUniqueUrl() {
-        return uniqueUrl;
-    }
-
-    public void setUniqueUrl(String uniqueUrl) {
-        this.uniqueUrl = uniqueUrl;
-    }
-
-    public Mail getEmail() {
-        return email;
-    }
-
-    public void setEmail(Mail email) {
-        this.email = email;
     }
 
     public Twitter getTwitter() {
@@ -275,12 +257,36 @@ public class OriginStampHash {
         this.twitter = twitter;
     }
 
-    public MultiSeed getMultiSeed() {
-        return multiSeed;
+    public String getUniqueUrl() {
+        return uniqueUrl;
     }
 
-    public void setMultiSeed(MultiSeed multiSeed) {
-        this.multiSeed = multiSeed;
+    public void setUniqueUrl(String uniqueUrl) {
+        this.uniqueUrl = uniqueUrl;
+    }
+
+    public String getHashString() {
+        return hashString;
+    }
+
+    public void setHashString(String hashString) {
+        this.hashString = hashString;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+
+    public Long getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Long dateCreated) {
+        this.dateCreated = dateCreated;
     }
 
     public SingleSeed getSingleSeed() {
@@ -289,5 +295,21 @@ public class OriginStampHash {
 
     public void setSingleSeed(SingleSeed singleSeed) {
         this.singleSeed = singleSeed;
+    }
+
+    public MultiSeed getMultiSeed() {
+        return multiSeed;
+    }
+
+    public void setMultiSeed(MultiSeed multiSeed) {
+        this.multiSeed = multiSeed;
+    }
+
+    public Boolean getCreated() {
+        return created;
+    }
+
+    public void setCreated(Boolean created) {
+        this.created = created;
     }
 }
